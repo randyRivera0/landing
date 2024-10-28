5e8f2dafa9833d77ea1783aec25cceacbfc87813
randyRivera0 <rariberm@espol.edu.ec>
2024-10-28 15:01:08 -0500

    modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
+// Reemplaza con tu URL
+const databaseURL = 'https://landing-57e2d-default-rtdb.firebaseio.com/coleccion.json'; 
+
 let ready = () => {
     console.log('DOM está listo')
     debugger
 }
 
 let loaded = (eventLoaded) => {
-    let myform = document.getElementById('form');
+    let myform = document.getElementById('form_email');
     myform.addEventListener('submit', (eventSubmit) => {
         eventSubmit.preventDefault(); 
 
@@ -12,24 +15,62 @@ let loaded = (eventLoaded) => {
            const emailText = emailElement.value;
 
            if (emailText.length === 0) {
-             emailElement.focus()
-           }
+            emailElement.focus()
+            emailElement.animate(
+                [
+                    { transform: "translateX(0)" },
+                    { transform: "translateX(50px)" },
+                    { transform: "translateX(-50px)" },
+                    { transform: "translateX(0)" }
+                ],
+                {
+                    duration: 400,
+                    easing: "linear",
+                }
+            )
+            
+
+            
+
+          }
+         
+          return;
+
     })
     console.log('Iframes e Images cargadas')
 }
 
+let sendData = ( form ) => {  
+
+    // Obtén los datos del formulario
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries()); // Convierte FormData a objeto
+
+    // new Date().toLocaleString( locales, options )
+    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' })
+
+    // Realiza la petición POST con fetch
+    fetch(databaseURL, {
+        method: 'POST', // Método de la solicitud
+        headers: {
+            'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
+        },
+        body: JSON.stringify(data) // Convierte los datos a JSON
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.statusText}`);
+        }
+        return response.json(); // Procesa la respuesta como JSON
+    })
+    .then(result => {
+        alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
+        form.reset()
+    })
+    .catch(error => {
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    });
+}
+
 window.addEventListener("DOMContentLoaded", ready);
 window.addEventListener("load", loaded)
-
-emailElement.animate(
-    [
-        { transform: "translateX(0)" },
-        { transform: "translateX(50px)" },
-        { transform: "translateX(-50px)" },
-        { transform: "translateX(0)" }
-    ],
-    {
-        duration: 400,
-        easing: "linear",
-    }
-)
