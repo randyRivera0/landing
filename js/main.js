4a8c21ea1d5a37eb6fd32f55cf9d64de5aa4dd19
randyRivera0 <rariberm@espol.edu.ec>
2024-11-06 12:34:47 -0500

    modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,45 +1,48 @@
 // Reemplaza con tu URL
-const databaseURL = 'https://landing-57e2d-default-rtdb.firebaseio.com/coleccion.json'; 
+const databaseURL = 'https://landing-57e2d-default-rtdb.firebaseio.com/collection.json'; 
 
 let ready = () => {
     console.log('DOM está listo')
-    debugger
+    
+    // Recuperación de datos
+    getData();
 }
 
 let loaded = (eventLoaded) => {
-    let myform = document.getElementById('form_email');
-    myform.addEventListener('submit', (eventSubmit) => {
+    let form = document.getElementById('form');
+
+    form.addEventListener('submit', (eventSubmit) => {
         eventSubmit.preventDefault(); 
 
         const emailElement = document.querySelector('.form-control-lg');
            const emailText = emailElement.value;
 
            if (emailText.length === 0) {
-            emailElement.focus()
-            emailElement.animate(
-                [
-                    { transform: "translateX(0)" },
-                    { transform: "translateX(50px)" },
-                    { transform: "translateX(-50px)" },
-                    { transform: "translateX(0)" }
-                ],
-                {
-                    duration: 400,
-                    easing: "linear",
-                }
-            )
-            
-
-            
-
-          }
+                emailElement.animate(
+                    [
+                        { transform: "translateX(0)" },
+                        { transform: "translateX(50px)" },
+                        { transform: "translateX(-50px)" },
+                        { transform: "translateX(0)" }
+                    ],
+                    {
+                        duration: 400,
+                        easing: "linear",
+                    }
+                )
+                emailElement.focus()
          
           return;
 
-    })
+        }
+
+        //Llamada a la función sendData()
+     	sendData(form);
+
+    });
     console.log('Iframes e Images cargadas')
 }
-
+/*
 let sendData = ( form ) => {  
 
     // Obtén los datos del formulario
@@ -71,6 +74,116 @@ let sendData = ( form ) => {
         alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
     });
 }
+*/
+
+let sendData = (form) => {
+    // Obtén los datos del formulario
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries()); // Convierte FormData a objeto
+
+    // new Date().toLocaleString( locales, options )
+    data['saved'] = new Date().toLocaleString('es-CO', { timeZone: 'America/Guayaquil' });
+
+    // Verificar los datos antes de enviarlos
+    console.log('Datos a enviar:', data);
+
+    // Realiza la petición POST con fetch
+    fetch(databaseURL, {
+        method: 'POST', // Método de la solicitud
+        headers: {
+            'Content-Type': 'application/json' // Especifica que los datos están en formato JSON
+        },
+        body: JSON.stringify(data) // Convierte los datos a JSON
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Error en la solicitud: ${response.statusText} (Código de estado: ${response.status})`);
+        }
+        return response.json(); // Procesa la respuesta como JSON
+    })
+    .then(result => {
+        console.log('Respuesta de la API:', result);
+        alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces');
+        form.reset();
+
+    // Recuperación de datos
+    getData()
+    
+    })
+
+    .catch(error => {
+        console.error('Error en la solicitud:', error);  // Detalles del error
+        alert('Hemos experimentado un error. ¡Vuelve pronto!');
+    });
+}
+
+let getData = async () => {  
+
+    try {
+
+        // Realiza la petición fetch a la URL de la base de datos
+        const response = await fetch(databaseURL, {
+            method: 'GET'
+        });
+
+        // Verifica si la respuesta es exitosa
+        if (!response.ok) {
+          alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+        }
+
+        // Convierte la respuesta en formato JSON
+        const data = await response.json();
+
+        if(data != null) {
+
+            // Cuente el número de suscriptores registrados por fecha a partir del objeto data
+
+            let countSuscribers = new Map()
+
+            if (Object.keys(data).length > 0) {
+                for (let key in data) {
+       
+                    let { email, saved } = data[key]
+                       
+                    let date = saved.split(",")[0]
+                       
+                    let count = countSuscribers.get(date) || 0;
+                    countSuscribers.set(date, count + 1)
+                }
+            }
+
+            // END
+
+            // Genere y agregue filas de una tabla HTML para mostrar fechas y cantidades de suscriptores almacenadas 
+
+            if (countSuscribers.size > 0) {
+
+                subscribers.innerHTML = ''
+       
+                let index = 1;
+                for (let [date, count] of countSuscribers) {
+                    let rowTemplate = `
+                        <tr>
+                            <th>${index}</th>
+                            <td>${date}</td>
+                            <td>${count}</td>
+                        </tr>`
+                    subscribers.innerHTML += rowTemplate
+                    index++;
+                }
+            }
+
+            // END
+
+        }
+
+      } catch (error) {
+        // Muestra cualquier error que ocurra durante la petición
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+      }
+
+}
+
 
 window.addEventListener("DOMContentLoaded", ready);
 window.addEventListener("load", loaded)
